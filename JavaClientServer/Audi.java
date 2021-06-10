ef134bf9aa978709f5493a0d6ab4262dc793e4e2
Bartłomiej Obudziński <obudzinskib@gmail.com>
2021-06-10 11:14:51 +0200

    Update Audi.java

--- JavaClientServer/Audi.java
+++ JavaClientServer/Audi.java
@@ -1,1 +1,12 @@
+import java.io.Serializable;
 
+public class Audi implements Serializable {
+    int modelYear;
+    String modelName;
+    public Audi(int year, String name) {
+        this.modelYear = year;
+        this.modelName = name;
+      }
+
+      
+}
